f2c7e6c4a57e33e6ef5adfc1c40483c35b333f21
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-03-10 15:38:51 +0100

    Update installation

--- Formula/dotcodegen.rb
+++ Formula/dotcodegen.rb
@@ -2,15 +2,15 @@ class Dotcodegen < Formula
   desc "Generate tests for your code using LLMs"
   homepage "https://github.com/ferrucc-io/dotcodegen"
   url "https://github.com/ferrucc-io/dotcodegen/archive/main.tar.gz"
-  version "0.1.1"
-  sha256 "80175adae5faa9bb7bad39c2b9d3fe8470f4d06029db5fa0bcdc10fa0a2ef916"
+  version "0.1.3"
+  sha256 "f4890a8e953f8d4e1e9152593d3a432a5e785fd25a11e712067f33d2c7665717"
 
   depends_on "ruby@3.3"
 
   def install
     system "gem", "build", "dotcodegen.gemspec"
-    system "gem", "install", "dotcodegen-0.1.1.gem"
-    bin.install "exe/run" => "codegen"
+    system "gem", "install", "dotcodegen-0.1.3.gem"
+    bin.install "exe/codegen" => "codegen"
   end
 
   test do
