b9999feca4a3de24aefefe8186f4943ebe0cf0fe
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-03-10 14:30:12 +0100

    Update dotcodegen.rb

--- Formula/dotcodegen.rb
+++ Formula/dotcodegen.rb
@@ -3,7 +3,7 @@ class Dotcodegen < Formula
   homepage "https://github.com/ferrucc-io/dotcodegen"
   url "https://github.com/ferrucc-io/dotcodegen/archive/main.tar.gz"
   version "0.1.0"
-  sha256 "e2282bde80c5a374201abc67d1bbad24647cd652b2de9b18273fa1aa8bdce181"
+  sha256 "25a9329d711d0ff3781c3e89592b4e3ad83f5eea21ef74c626549959dfc6e651"
 
   depends_on "ruby@3.3"
 
